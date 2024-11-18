c1307adafa61eff7c0a1314a576be4dc1c2b8d2f
Neema <neemamwende009@gmail.com>
2024-11-18 13:20:29 +0300

    ucheckusername route

--- feedback-app/src/app/layout.tsx
+++ feedback-app/src/app/layout.tsx
@@ -1,17 +1,11 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Inter } from "next/font/google";
 import "./globals.css";
+import AuthProvider from "./context/AuthProvider";
+import { Toaster } from "@/components/ui/toaster"
+import Navbar from "@/components/Navbar";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,12 +18,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="en" className="bg-black">
+      <AuthProvider>
+      <body className={`${inter.className}`}>
+      <Navbar/>
+
         {children}
-      </body>
+        <Toaster/>
+        </body>
+        </AuthProvider>
     </html>
   );
 }
